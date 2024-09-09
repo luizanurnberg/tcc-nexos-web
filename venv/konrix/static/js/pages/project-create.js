893d9f7084e25cef1620c6f170ae1bdcba486ea8
luizanurnberg <nurnbergluiza@gmail.com>
2024-09-08 21:55:18 -0300

    feat(requirements table): add requirements table

--- venv/konrix/static/js/pages/project-create.js
+++ venv/konrix/static/js/pages/project-create.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const requirementsArray = [];
     const elements = {
         addRequirementsBtn: document.getElementById('btn-add-requirements'),
@@ -16,21 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
         projectName: document.getElementById('project-name'),
         projectDescription: document.getElementById('project-description'),
         projectBudget: document.getElementById('project-budget'),
-        projectSelectTime: document.getElementById('project-select-time')
+        projectSelectTime: document.getElementById('project-select-time'),
+        modalBody: document.getElementById('modal-table'),
+        addRequirementsDependenciesBtn: document.getElementById('btn-add-requirements'),
     };
 
     elements.addRequirementsBtn.addEventListener('click', toggleRequirementDiv);
     elements.saveRequirementBtn.addEventListener('click', addRequirement);
     elements.cancelRequirementBtn.addEventListener('click', clearRequirementFields);
-    elements.cancelBtn.addEventListener('click', () => console.log("Botão 'Cancelar' clicado"));
-    elements.saveBtn.addEventListener('click', saveProject);
+    //elements.cancelBtn.addEventListener('click', () => console.log("Botão 'Cancelar' clicado"));
+    //elements.saveBtn.addEventListener('click', saveProject);
 
     // Função para mostrar/ocultar a div de requisitos
     function toggleRequirementDiv() {
-        elements.requirementDiv.style.display = 
-            elements.requirementDiv.style.display === 'none' || elements.requirementDiv.style.display === '' 
-            ? 'block' 
-            : 'none';
+        elements.requirementDiv.style.display =
+            elements.requirementDiv.style.display === 'none' || elements.requirementDiv.style.display === ''
+                ? 'block'
+                : 'none';
     }
 
     // Função para limpar os campos de requisitos
@@ -45,6 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Função para adicionar um requisito ao array
     function addRequirement() {
+        requirementCounter++;
         const requirement = {
             name: elements.requirementName.value,
             description: elements.requirementDescription.value,
@@ -56,6 +59,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
         requirementsArray.push(requirement);
         clearRequirementFields();
+        updateModalContent();
+    }
+
+    // Função para atualizar o conteúdo do modal
+    function updateModalContent() {
+        const thead = document.getElementById('modal-thead');
+        const tbody = document.getElementById('modal-tbody');
+
+        thead.querySelector('tr').innerHTML = '<th scope="col" class="py-3 ps-4"></th>';
+
+        tbody.innerHTML = '';
+
+        if (requirementsArray.length > 0) {
+            requirementsArray.forEach(req => {
+                thead.querySelector('tr').innerHTML += `<th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">${req.name}</th>`;
+            });
+
+            requirementsArray.forEach((req, rowIndex) => {
+                let row = `<tr><td class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">${req.name}</td>`;
+                requirementsArray.forEach((_, colIndex) => {
+                    row += `<td class="py-3 ps-4"><input type="checkbox" id="checkbox-${rowIndex}-${colIndex}" class="form-checkbox rounded"></td>`;
+                });
+                row += '</tr>';
+                tbody.innerHTML += row;
+            });
+        } else {
+            tbody.innerHTML = '<tr><td colspan="100%" class="py-3 text-center text-gray-500">Nenhum requisito adicionado. Adicione requisitos para ver a matriz de dependências.</td></tr>';
+        }
     }
 
     // Função para salvar os dados do projeto
@@ -75,14 +106,14 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             body: JSON.stringify(data)
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Sucesso:', data);
-            // Lógica adicional após o envio bem-sucedido
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-            // Lógica adicional para tratamento de erro
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log('Sucesso:', data);
+                // Lógica adicional após o envio bem-sucedido
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+                // Lógica adicional para tratamento de erro
+            });
     }
 });
