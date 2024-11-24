214123ede3aef1bb411a64826885a7a6c9595179
luizanurnberg <nurnbergluiza@gmail.com>
2024-11-24 15:09:28 -0300

    feat(improvements): add new improvements

--- venv/konrix/static/js/pages/apps-kanban.js
+++ venv/konrix/static/js/pages/apps-kanban.js
@@ -8,17 +8,32 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
             if (data && data.data) {
-                console.log(data.data);
+
+                function getPriorityHtml(weight) {
+                    if (weight === 25) {
+                        return '<span class="text-gray-500"><i class="mgc_snow_line"></i> Prioridade Baixa</span>';
+                    } else if (weight === 50) {
+                        return '<span class="text-gray-500"><i class="mgc_pin_line"></i> Prioridade Média</span>';
+                    } else if (weight === 100) {
+                        return '<span class="text-gray-500"><i class="mgc_fire_line"></i> Prioridade Alta</span>';
+                    } else {
+                        return '<span class="text-gray-500"><i class="icon-unknown-priority"></i> Sem Prioridade</span>';
+                    }
+                }
 
                 data.data.REQUIREMENT.forEach(function (req) {
+                    const priorityHtml = getPriorityHtml(req.WEIGHT);
                     const dependencyLabel = req.DEPENDENTS && req.DEPENDENTS.length > 0
-                        ? '<h4 class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-warning bg-warning/25 rounded-full">Depende do ...</h4>'
+                        ? `<h4 class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-warning bg-warning/25 rounded-full" style="margin-left: -20%">
+                        Depende do ${req.DEPENDENTS.map(req => `#${req.ID}`).join(', ')}
+                        </h4>`
                         : '';
 
                     const cardHtml = `
-                        <div class="card p-4 cursor-pointer">
+                        <div class="card p-4 break-words">
                             <div class="flex justify-between items-center">
-                                <h4 class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-danger bg-danger/25 rounded-full">
+                                <h4
+                                    class="flex justify-between items-center h-6 px-5 text-xs font-semibold text-danger bg-danger/25 rounded-full">
                                 </h4>
                                 ${dependencyLabel}
                                 <div class="text-xs">#${req.ID}</div>
@@ -26,12 +41,12 @@ $(document).ready(function () {
                             <h4 class="mt-3 text-sm">${req.NAME}</h4>
                             <div class="flex items-center w-full mt-3 text-xs font-medium text-gray-400">
                                 <div class="flex items-center">
-                                    <i class="mgc_chat_3_line text-base"></i>
-                                    <span class="ms-1 leading-none">${req.comments_count}</span>
+                                    <span class="ms-1 leading-none">${req.DESCRIPTION || 'Sem descrição'}</span>
                                 </div>
-                                <div class="flex items-center ms-4">
-                                    <i class="mgc_attachment_line rotate-45 text-base"></i>
-                                    <span class="ms-1 leading-none">${req.attachments_count}</span>
+                            </div>
+                            <div class="flex items-center w-full mt-3 text-xs font-medium text-gray-400">
+                                <div class="flex items-center">
+                                    <span class="ms-1 leading-none">${priorityHtml}</span>
                                 </div>
                             </div>
                         </div>
@@ -39,16 +54,19 @@ $(document).ready(function () {
                     $('#kanbanborad-two').append(cardHtml);
                 });
 
-                data.data.REQUIREMENT_TO_IMPLEMENT.forEach(function (req) {
+                data.data.REQUIREMENT_TO_IMPLEMENT.sort((a, b) => b.WEIGHT - a.WEIGHT).forEach(function (req) {
+                    const priorityHtml = getPriorityHtml(req.WEIGHT);
                     const dependencyLabel = req.DEPENDENTS && req.DEPENDENTS.length > 0
-                        ? '<h4 class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-warning bg-warning/25 rounded-full">Depende do ...</h4>'
+                        ? `<h4 class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-warning bg-warning/25 rounded-full" style="margin-left: -20%">
+                        Depende do ${req.DEPENDENTS.map(req => `#${req.ID}`).join(', ')}
+                        </h4>`
                         : '';
 
                     const cardHtml = `
-                        <div class="card p-4 cursor-pointer">
+                        <div class="card p-4 break-words">
                             <div class="flex justify-between items-center">
                                 <h4
-                                    class="flex justify-between items-center h-6 px-3 text-xs font-semibold text-success bg-success/25 rounded-full">
+                                    class="flex justify-between items-center h-6 px-5 text-xs font-semibold text-success bg-success/25 rounded-full">
                                 </h4>
                                 ${dependencyLabel}
                                 <div class="text-xs">#${req.ID}</div>
@@ -56,76 +74,22 @@ $(document).ready(function () {
                             <h4 class="mt-3 text-sm">${req.NAME}</h4>
                             <div class="flex items-center w-full mt-3 text-xs font-medium text-gray-400">
                                 <div class="flex items-center">
-                                    <i class="mgc_chat_3_line text-base"></i>
-                                    <span class="ms-1 leading-none">${req.comments_count}</span>
+                                    <span class="ms-1 leading-none">${req.DESCRIPTION || 'Sem descrição'}</span>
                                 </div>
-                                <div class="flex items-center ms-4">
-                                    <i class="mgc_attachment_line rotate-45 text-base"></i>
-                                    <span class="ms-1 leading-none">${req.attachments_count}</span>
+                            </div>
+                            <div class="flex items-center w-full mt-3 text-xs font-medium text-gray-400">
+                                <div class="flex items-center">
+                                    <span class="ms-1 leading-none">${priorityHtml}</span>
                                 </div>
                             </div>
                         </div>
                     `;
                     $('#kanbanborad-one').append(cardHtml);
                 });
-
-                initializeSortable();
             }
         },
         error: function (xhr, status, error) {
             console.error("Erro na requisição AJAX:", error);
         },
     });
-
-
-    function initializeSortable() {
-        const kanbanOne = document.getElementById('kanbanborad-one');
-        const kanbanTwo = document.getElementById('kanbanborad-two');
-        const kanbanThree = document.getElementById('kanbanborad-three');
-        const kanbanFour = document.getElementById('kanbanborad-four');
-        const kanbanFive = document.getElementById('kanbanborad-five');
-        const kanbanSix = document.getElementById('kanbanborad-six');
-
-        if (kanbanOne) {
-            new Sortable(kanbanOne, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-
-        if (kanbanTwo) {
-            new Sortable(kanbanTwo, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-
-        if (kanbanThree) {
-            new Sortable(kanbanThree, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-
-        if (kanbanFour) {
-            new Sortable(kanbanFour, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-
-        if (kanbanFive) {
-            new Sortable(kanbanFive, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-
-        if (kanbanSix) {
-            new Sortable(kanbanSix, {
-                group: 'shared',
-                animation: 150,
-            });
-        }
-    }
 });
--- venv/konrix/static/js/pages/project-list.js
+++ venv/konrix/static/js/pages/project-list.js
@@ -27,65 +27,67 @@ $(document).ready(function () {
         projects.forEach(project => {
             let statusClass = '';
             let extraClass = '';
+            let isClickable = true;
+
             if (project.STATUS.ID === 1) {
                 statusClass = 'bg-yellow-500'; // Amarelo
+                isClickable = false; // Desabilitar clique
             } else if (project.STATUS.ID === 2) {
                 statusClass = 'bg-green-500'; // Verde
                 extraClass = 'px-3 py-1.5'; // Aumentar tamanho
             } else if (project.STATUS.ID === 3) {
                 statusClass = 'bg-red-500'; // Vermelho
+                isClickable = false;
             }
 
             const projectHtml = `
-        <div class="card" data-id="${project.ID}">
-                <div class="card-header">
-                    <div class="flex justify-between items-center">
-                        <a href="/apps/kanban/${project.ID}">
-                            <h5 class="card-title">${project.TITLE}</h5>
-                        </a>
-                        <i class="mgc_delete_2_fill cursor-pointer text-red-500 delete-icon">
-                            <div id="MessageSweetAlert" class="hidden w-full overflow-hidden transition-[height] duration-300">
-                                <pre class="language-html h-auto">
-                                    <code>
-                                        &lt;button type=&quot;button&quot; class=&quot;btn bg-success text-white&quot; id=&quot;sweetalert-success&quot;&gt;Success&lt;/button&gt;
-                                    </code>
-                                </pre>
-                            </div>
-                        </i>
-                    </div>
-                </div>
-                <div class="flex flex-col">
-                <div class="py-3 px-6">
-                <a href="/apps/kanban/${project.ID}">
-                        <h5 class="my-2"><a href="#" class="text-slate-900 dark:text-slate-200">${project.DESCRIPTION || 'Sem descrição'}</a></h5>
-                        </a>
-                    </div>
-                </div>
-                <div class="border-t p-5 border-gray-300 dark:border-gray-700">
-                    <div class="grid lg:grid-cols-2 gap-4">
-                        <div class="flex items-center justify-between gap-2">
-                            <a href="#" class="text-sm">
-                                <i class="mgc_calendar_line text-lg me-2"></i>
-                                <span class="align-text-bottom">${formatDate(project.CREATED_AT.$date)}</span>
-                            </a>
-                            <a href="#" class="text-sm">
-                                <i class="mgc_align_justify_line text-lg me-2"></i>
-                                <span class="align-text-bottom">${project.REQUIREMENT.length} cards ao total</span>
-                            </a>
-                        </div>
-                        <div class="flex justify-between items-center" style="margin-left: 10%">
-                        <a href="#" class="text-sm">
-                                <i class="mgc_pig_money_line text-lg me-2"></i>
-                                <span class="align-text-bottom">R$: ${project.TOTAL_BUDGET}</span>
-                        </a>
-                            <div class="${statusClass} text-xs text-white rounded-md py-1 px-4 font-medium" role="alert">
-                                <span>${project.STATUS.NAME}</span>
-                            </div>
+                <div class="card ${isClickable ? 'clickable' : 'non-clickable'}" data-id="${project.ID}">
+                    <div class="card-header">
+                        <div class="flex justify-between items-center">
+                            ${isClickable ? `
+                                <a href="/apps/kanban/${project.ID}">
+                                    <h5 class="card-title">${project.TITLE}</h5>
+                                </a>` : `
+                                <h5 class="card-title text-gray-500">${project.TITLE}</h5>
+                            `}
+                            <i class="mgc_delete_2_fill cursor-pointer text-red-500 delete-icon"></i>
                         </div>
                     </div>
+                    <div class="flex flex-col">
+                        <div class="py-3 px-6">
+                            <h5 class="my-2">
+                                ${isClickable ? `
+                                    <a href="/apps/kanban/${project.ID}" class="text-slate-900 dark:text-slate-200">${project.DESCRIPTION || 'Sem descrição'}</a>` : `
+                                    <span class="text-gray-500">${project.DESCRIPTION || 'Sem descrição'}</span>
+                                `}
+                            </h5>
+                        </div>
+                    </div>
+                    <div class="border-t p-5 border-gray-300 dark:border-gray-700">
+                        <div class="grid lg:grid-cols-2 gap-4">
+                            <div class="flex items-center justify-between gap-2">
+                                <a class="text-sm">
+                                    <i class="mgc_calendar_line text-lg me-2"></i>
+                                    <span class="align-text-bottom">${formatDate(project.CREATED_AT.$date)}</span>
+                                </a>
+                                <a class="text-sm">
+                                    <i class="mgc_align_justify_line text-lg me-2"></i>
+                                    <span class="align-text-bottom">${project.REQUIREMENT.length} cards ao total</span>
+                                </a>
+                            </div>
+                            <div class="flex justify-between items-center" style="margin-left: 10%">
+                                <a class="text-sm">
+                                    <i class="mgc_pig_money_line text-lg me-2"></i>
+                                    <span class="align-text-bottom">R$: ${project.TOTAL_BUDGET}</span>
+                                </a>
+                                <div class="${statusClass} text-xs text-white rounded-md py-1 px-4 font-medium" role="alert">
+                                    <span>${project.STATUS.NAME}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-        </div>
-    `;
+            `;
             container.append(projectHtml);
         });
 
