75737331b27875f2773b2b85ccecf0c03c03c654
luizanurnberg <nurnbergluiza@gmail.com>
2025-03-10 17:20:25 -0300

    feat(client): add client page

--- venv/konrix/static/js/config.js
+++ venv/konrix/static/js/config.js
@@ -25,7 +25,7 @@
         sidenav: {
             view: "hover"  
         },
-        api: "http://192.168.1.3:3000"
+        api: "http://192.168.1.2:3000"
     };
 
     const html = document.getElementsByTagName("html")[0];
